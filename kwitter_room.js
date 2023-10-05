8204d00eb358223727f1e886f16964df38180e11
BrentByju <130359282+BrentByju@users.noreply.github.com>
2023-10-05 20:55:37 +1000

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-bzQYj-5JQEglybrzOWX7zUGF-XwQJaI",
+  authDomain: "twitter-941b8.firebaseapp.com",
+  projectId: "twitter-941b8",
+  storageBucket: "twitter-941b8.appspot.com",
+  messagingSenderId: "170371931209",
+  appId: "1:170371931209:web:af19e05efb2a8bbd73b398",
+  measurementId: "G-C64HGSWC2Y"
+};
+
